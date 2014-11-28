90af5255905e3b4168902b2ea41e012dd0289de6
mputters <michael.putters@binarygenetics.com>
2014-11-28 08:59:46 +0100

    Added TypeScript definition file

---
+++ typings/angular-gamepad/angular-gamepad.d.ts
@@ -0,0 +1,18 @@
+// Type definitions for Angular Gamepad 0.1.0+
+// Project: https://github.com/pcx360/angular-gamepad
+// Definitions by: Michael Putters <https://github.com/mputters>
+
+/// <reference path="../angularjs/angular.d.ts"/>
+
+declare module ng.gamepad {
+    interface IGamepad {
+
+    }
+
+    interface IGamepadProvider extends ng.IServiceProvider {
+
+    }
+
+    interface IGamepadController {
+    }
+}
